cbef6367c1fc12f568914da63c80a35d29b4ce01
Danila Bolshakov <danila1999777@gmail.com>
2023-10-18 14:29:24 +0300

    Initial commit plus Update SceneDelegate

---
+++ ClimaCodeMVP/App/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  ClimaCodeMVP
+//
+//  Created by iOS - Developer on 18.10.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigatorBar = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigatorBar
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+    func sceneWillResignActive(_ scene: UIScene) { }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ ClimaCodeMVP/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ClimaCodeMVP
+//
+//  Created by iOS - Developer on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
